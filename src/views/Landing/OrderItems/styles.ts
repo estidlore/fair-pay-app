0802b2a49ddfc0965a181953910cd016d34628e2
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-01-17 14:10:53 -0500

    feat: #33 render orderitems by columns (#34)
    - Render OrderItems by column instead of by row
    - Shorten column's header name

--- src/views/Landing/OrderItems/styles.ts
+++ src/views/Landing/OrderItems/styles.ts
@@ -1,26 +1,33 @@
+import { colors } from "appStyles";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  column: {
+    flexBasis: "auto",
+    flexGrow: 1,
+    flexShrink: 1
+  },
   container: {
+    borderBottomColor: colors.light,
+    borderBottomWidth: 2,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    marginBottom: 8,
+    paddingBottom: 8
   },
   element: {
     fontSize: 16,
-    marginVertical: 4,
-    width: "25%"
+    marginVertical: 4
   },
   fallback: {
-    fontSize: 16,
-    marginVertical: 4
+    width: "100%"
   },
   header: {
     fontSize: 18,
     fontWeight: "bold",
-    marginVertical: 4,
-    width: "25%"
+    marginVertical: 4
   }
 });
 
